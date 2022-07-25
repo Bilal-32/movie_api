b9368f327015aca332e912306b01409f11afed55
Bilal Hussain <105238891+Bilal-32@users.noreply.github.com>
2022-07-25 15:35:25 +0100

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,56 +1,49 @@
-onst passport = require('passport'),
-  LocalStrategy = require('passport-local').Strategy,
-  Models = require('./models.js'),
-  passportJWT = require('passport-jwt');
+const passport = require('passport'),
+    localStrategy = require('passport-local').Strategy,
+    Models = require('./model.js'),
+    passportJWT = require('passport-jwt');
 
 let Users = Models.User,
-  JWTStrategy = passportJWT.Strategy,
-  ExtractJWT = passportJWT.ExtractJwt;
+    JWTStrategy = passportJWT.Strategy,
+    ExtractJWT = passportJWT.ExtractJwt;
 
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: 'Username',
-      passwordField: 'Password',
-    },
-    (username, password, callback) => {
-      console.log(username + '  ' + password);
-      Users.findOne({ Username: username }, (error, user) => {
+// Defines baisc HTTP authentication for login request
+passport.use(new localStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+}, (username, password, callback) => {
+    console.log(username + ' ' + password);
+    Users.findOne({ username: username }, (error, user) => {
         if (error) {
-          console.log(error);
-          return callback(error);
+            console.log(error);
+            return callback(error);
         }
 
         if (!user) {
-          console.log('incorrect username');
-          return callback(null, false, { message: 'Incorrect username or password.' });
+            console.log('Incorrect username');
+            return callback(null, false, { message: 'Incorrect username or password' });
         }
 
         if (!user.validatePassword(password)) {
-          console.log('incorrect password');
-          return callback(null, false, { message: 'Incorrect password.' });
+            console.log('incorrect password');
+            return callback(null, false, { message: 'Incorrect password.' });
         }
-        console.log('finished');
-        return callback(null, user);
-      });
-    }
-  )
-);
 
-passport.use(
-  new JWTStrategy(
-    {
-      jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'your_jwt_secret',
-    },
-    (jwtPayload, callback) => {
-      return Users.findById(jwtPayload._id)
+        console.log('Finished');
+        return callback(null, user);
+    });
+}));
+
+// Allows to authenticate users based on the JWT submitted alongside their request.
+passport.use(new JWTStrategy({
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'your_jwt_secret'
+}, (jwtPayload, callback) => {
+    return Users.findById(jwtPayload._id)
         .then((user) => {
-          return callback(null, user);
+            return callback(null, user);
         })
         .catch((error) => {
-          return callback(error);
+            return callback(error)
         });
-    }
-  )
-);
+}));
